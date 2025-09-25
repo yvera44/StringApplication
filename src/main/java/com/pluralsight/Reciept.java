66e67d973b04ed318403a01dded9a96c1c93bb63
YairVera <yairvera44@gmail.com>
2025-09-25 11:04:04 -0500

    VariableApp6

---
+++ src/main/java/com/pluralsight/Reciept.java
@@ -0,0 +1,14 @@
+package com.pluralsight;
+
+public class Reciept {
+    public static void main(String[] arg){
+
+        String items = "iPhones";
+        double iPhonePrice = 1099.99;
+        int quantity = 3;
+
+        System.out.println("You bought " + quantity + " " + items + " for $" + iPhonePrice);
+
+
+    }
+}
