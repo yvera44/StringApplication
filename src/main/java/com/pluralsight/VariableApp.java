0b32f15ea1aabe4dc9cffda8f70fbe2c01865ffa
YairVera <yairvera44@gmail.com>
2025-09-25 10:39:55 -0500

    VariableApp3

--- src/main/java/com/pluralsight/VariableApp.java
+++ src/main/java/com/pluralsight/VariableApp.java
@@ -7,7 +7,13 @@ public class VariableApp {
     int year = 2025;
     String middleInitial = "N/A";
     Boolean pets = true;
-    String message = "Hey twin";
+    String message = "Tung tung tung tung tun sahur!";
+
+    System.out.println("My favorite color is " + color);
+    System.out.println("Year I Started: " + year);
+    System.out.println("My middle initial " + middleInitial);
+    System.out.println("I have a pet " + pets);
+    System.out.println("My message to you: " + message);
 
 
 
