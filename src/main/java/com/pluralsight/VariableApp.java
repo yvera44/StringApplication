676feffc686e217982d849a565b279457259b365
YairVera <yairvera44@gmail.com>
2025-09-25 10:47:59 -0500

    VariableApp4

--- src/main/java/com/pluralsight/VariableApp.java
+++ src/main/java/com/pluralsight/VariableApp.java
@@ -3,6 +3,7 @@ package com.pluralsight;
 public class VariableApp {
     public static void main(String[] args){
 
+        //Step one
     String color = "Green";
     int year = 2025;
     String middleInitial = "N/A";
@@ -14,6 +15,17 @@ public class VariableApp {
     System.out.println("My middle initial " + middleInitial);
     System.out.println("I have a pet " + pets);
     System.out.println("My message to you: " + message);
+
+    //Step Two
+   int daysInWeek = 7;
+   double coffeePrice = 2.99;
+   String favLetter = "Y";
+   Boolean itIsRaining = false;
+
+   System.out.println("There are " + 7 + " days in a week.");
+   System.out.println("A cup of Coffee is $" + coffeePrice);
+   System.out.println("My favorite letter is " + favLetter);
+   System.out.println("It is currently raining: " + itIsRaining);
 
 
 
