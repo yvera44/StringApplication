e4e9d761d23a3202c0a86b2af2147037e47fcc6e
YairVera <yairvera44@gmail.com>
2025-09-25 10:26:29 -0500

    VariableApp2

--- src/main/java/com/pluralsight/VariableApp.java
+++ src/main/java/com/pluralsight/VariableApp.java
@@ -3,5 +3,13 @@ package com.pluralsight;
 public class VariableApp {
     public static void main(String[] args){
 
+    String color = "Green";
+    int year = 2025;
+    String middleInitial = "N/A";
+    Boolean pets = true;
+    String message = "Hey twin";
+
+
+
     }
 }
